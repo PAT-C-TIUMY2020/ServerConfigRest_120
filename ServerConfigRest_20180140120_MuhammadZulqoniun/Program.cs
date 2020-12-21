9838ff7af1a0c109c3d22fc1f47cfd4027325542
Zulqoniun <muhammadqoni123@gmail.com>
2020-12-21 20:06:16 +0700

    Add project files.

---
+++ ServerConfigRest_20180140120_MuhammadZulqoniun/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140120_MuhammadZulqoniun;
+
+
+namespace ServerConfigRest_20180140120_MuhammadZulqoniun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
